00d51362630de3dac6ecd2b17bbd0c2abac39263
AnilAkar <anlakr6@gmail.com>
2023-09-06 15:09:04 +0300

    Initial commit, end project

---
+++ src/components/App.js
@@ -0,0 +1,142 @@
+import { useEffect, useReducer } from "react";
+import DateCounter from "./DateCounter.js";
+import Header from "./Header.js";
+import Main from "./Main.js";
+import Loader from "./Loader.js";
+import Error from "./Error.js";
+import StartScreen from "./StartScreen.js";
+import Question from "./Question.js";
+import NextButton from "./NextButton.js";
+import Progress from "./Progress.js";
+import FinishScreen from "./FinishScreen.js";
+import Timer from "./Timer.js";
+
+const SECS_PER_QUESTION = 30;
+
+const initialState = {
+  questions: [],
+  status: "loading",
+  index: 0,
+  answer: null,
+  points: 0,
+  highscore: 0,
+  remainingSeconds: 0,
+};
+
+function reducer(state, action) {
+  switch (action.type) {
+    case "dataRecieved":
+      return { ...state, questions: action.payload, status: "ready" };
+    case "dataFailed":
+      return { ...state, status: "error" };
+    case "start":
+      return {
+        ...state,
+        status: "active",
+        remainingSeconds: state.questions.length * SECS_PER_QUESTION,
+      };
+    case "newAnswer":
+      const curQuestion = state.questions.at(state.index);
+
+      return {
+        ...state,
+        answer: action.payload,
+        points:
+          action.payload === curQuestion.correctOption
+            ? state.points + curQuestion.points
+            : state.points,
+      };
+    case "nextQuestion":
+      return { ...state, index: state.index + 1, answer: null };
+    case "finish":
+      return {
+        ...state,
+        status: "finished",
+        highscore:
+          state.points > state.highscore ? state.points : state.highscore,
+      };
+
+    case "restart":
+      return {
+        ...initialState,
+        questions: state.questions,
+        status: "ready",
+      };
+
+    case "tick":
+      return {
+        ...state,
+        remainingSeconds: state.remainingSeconds - 1,
+        status: state.remainingSeconds === 0 ? "finished" : state.status,
+      };
+
+    default:
+      throw new Error("unknown action");
+  }
+}
+
+export default function App() {
+  const [
+    { questions, status, index, answer, points, highscore, remainingSeconds },
+    dispatch,
+  ] = useReducer(reducer, initialState);
+
+  const numQuestions = questions.length;
+  const maxPoints = questions.reduce(
+    (accumulator, currentValue) => accumulator + currentValue.points,
+    0
+  );
+
+  useEffect(function () {
+    fetch(`http://localhost:9000/questions`)
+      .then((res) => res.json())
+      .then((data) => dispatch({ type: "dataRecieved", payload: data }))
+      .catch(() => dispatch({ type: "dataFailed" }));
+  }, []);
+
+  return (
+    <div className="app">
+      <Header />
+      <Main>
+        {status === "loading" && <Loader />}
+        {status === "error" && <Error />}
+        {status === "ready" && (
+          <StartScreen numQuestions={numQuestions} dispatch={dispatch} />
+        )}
+        {status === "active" && (
+          <>
+            <Progress
+              index={index}
+              numQuestions={numQuestions}
+              points={points}
+              maxPoints={maxPoints}
+              answer={answer}
+            />
+            <Question
+              questions={questions[index]}
+              answer={answer}
+              dispatch={dispatch}
+            />
+            <footer className="footer">
+              <Timer dispatch={dispatch} remainingSeconds={remainingSeconds} />
+              <NextButton
+                dispatch={dispatch}
+                answer={answer}
+                index={index}
+                numQuestions={numQuestions}
+              />
+            </footer>
+          </>
+        )}
+        {status === "finished" && (
+          <FinishScreen
+            points={points}
+            maxPoints={maxPoints}
+            highscore={highscore}
+            dispatch={dispatch}
+          />
+        )}
+      </Main>
+    </div>
+  );
+}
